7fb37fccf72c65fd02a04317683aaa128ef83a1d
张鲁贤 <177782633@qq.com>
2023-07-19 16:26:34 +0800

    员工管理页面完整实现

--- src/components/staff/PortList.vue
+++ src/components/staff/PortList.vue
@@ -8,22 +8,44 @@
     <template #bodyCell="{ column, record }">
       <template v-if="column.key === 'action'">
         <span>
-          <button>编辑</button>
+          <button @click="editPort(record)">编辑</button>
           <button @click="delUser(record.id)">删除</button>
         </span>
       </template>
     </template>
   </a-table>
-  <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
+  <a-modal v-model:open="open" :title="isEdit" @ok="handleOk">
+    <a-form ref="formRef" :model="formState" name="basic" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" autocomplete="off">
+      <a-form-item label="部门名称" name="name" :rules="[{ required: true, message: 'Please input your name!' }]">
+        <a-input v-model:value="formState.name" />
+      </a-form-item>
+      <a-form-item label="人数" name="num" :rules="[{ required: true, message: 'Please input your num!' }]">
+        <a-input v-model:value="formState.num" />
+      </a-form-item>
+      <a-form-item label="负责人" name="charge" :rules="[{ required: true, message: 'Please input your charge!' }]">
+        <a-input v-model:value="formState.charge" />
+      </a-form-item>
+      <a-form-item label="职务" name="job" :rules="[{ required: true, message: 'Please input your job!' }]">
+        <a-input v-model:value="formState.job" />
+      </a-form-item>
+      <a-form-item label="手机号" name="tel" :rules="[{ required: true, message: 'Please input your tel!' }]">
+        <a-input v-model:value="formState.tel" />
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue"
 import request from "../../utils/request"
+interface FormState {
+  id?: number
+  name: string
+  num?: number
+  charge: string
+  job: string
+  tel: string
+}
 const columns = [
   {
     title: "部门名称",
@@ -50,19 +72,44 @@ const columns = [
     key: "action",
   },
 ]
+const formRef = ref()
+const isEdit = ref<string>("新增")
 const open = ref<boolean>(false)
+const formState = ref<FormState>({
+  name: "",
+  charge: "",
+  job: "",
+  tel: "",
+})
 const showModal = () => {
+  formState.value = { name: "", charge: "", job: "", tel: "" }
+  isEdit.value = "新增"
   open.value = true
 }
-const handleOk = (e: MouseEvent) => {
-  console.log(e)
-  open.value = false
+const handleOk = async () => {
+  formRef.value
+    .validateFields()
+    .then(async (res: FormState) => {
+      console.log(res, "ff")
+      open.value = false
+      if (isEdit.value == "新增") {
+        addPort()
+        getUserList()
+      } else {
+        let result = await request.post("/api/staff/portEdit", formState.value)
+        console.log(result)
+        getUserList()
+      }
+    })
+    .catch((err: any) => {
+      console.log(err)
+    })
 }
 const data = ref([])
 const searchVal = ref<string>("")
 const onSearch = async (searchValue: string) => {
   console.log("or use this.value", searchVal.value)
-  let result = await request.get("/api/user/search", {
+  let result = await request.get("/api/staff/portSearch", {
     params: {
       searchVal: searchValue,
     },
@@ -71,18 +118,26 @@ const onSearch = async (searchValue: string) => {
   data.value = result.data.data
 }
 const getUserList = async () => {
-  let result = await request.get("/api/staff/list")
-  console.log(result)
+  let result = await request.get("/api/staff/portList")
   data.value = result.data.data
 }
-const delUser = async (uid: number) => {
-  let result = await request.get("/api/user/delete", {
+const delUser = async (id: number) => {
+  let result = await request.get("/api/staff/portDel", {
     params: {
-      id: uid,
+      id,
     },
   })
   console.log(result)
   getUserList()
+}
+const editPort = async (value: FormState) => {
+  isEdit.value = "编辑"
+  open.value = true
+  formState.value = value
+}
+const addPort = async () => {
+  let result = await request.post("/api/staff/portAdd", formState.value)
+  console.log(result)
 }
 const reset = () => {
   searchVal.value = ""
--- src/components/staff/StaffList.vue
+++ src/components/staff/StaffList.vue
@@ -2,44 +2,126 @@
   <div class="search">
     搜索:<a-input-search v-model:value="searchVal" placeholder="请输入员工名称" style="width: 200px" @search="onSearch" />
     <a-button type="primary" @click="reset">重置</a-button>
-    <a-button type="primary">添加员工</a-button>
+    <a-button type="primary" @click="showModal">新增员工</a-button>
   </div>
   <a-table :columns="columns" :data-source="data">
     <template #bodyCell="{ column, record }">
       <template v-if="column.key === 'action'">
         <span>
-          <button @click="delUser(record.id)">删除</button>
+          <button @click="editStaff(record)">编辑</button>
+          <button @click="delStaff(record.id)">删除</button>
         </span>
       </template>
     </template>
   </a-table>
+  <a-modal v-model:open="open" :title="isEdit" @ok="handleOk">
+    <a-form ref="formRef" :model="formState" name="basic" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" autocomplete="off">
+      <a-form-item label="部门选择" name="port" :rules="[{ required: true, message: 'Please input your name!' }]">
+        <a-input v-model:value="formState.port" />
+      </a-form-item>
+      <a-form-item label="姓名" name="name" :rules="[{ required: true, message: 'Please input your num!' }]">
+        <a-input v-model:value="formState.name" />
+      </a-form-item>
+      <a-form-item label="工号" name="jobNum" :rules="[{ required: true, message: 'Please input your charge!' }]">
+        <a-input v-model:value="formState.jobNum" />
+      </a-form-item>
+      <a-form-item label="职务" name="job" :rules="[{ required: true, message: 'Please input your job!' }]">
+        <a-input v-model:value="formState.job" />
+      </a-form-item>
+      <a-form-item label="手机号" name="tel" :rules="[{ required: true, message: 'Please input your tel!' }]">
+        <a-input v-model:value="formState.tel" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue"
 import request from "../../utils/request"
+interface FormState {
+  id?: number
+  name: string
+  jobNum?: string
+  port: string
+  job: string
+  tel: string
+  password?: string
+}
 const columns = [
+  {
+    title: "部门名称",
+    dataIndex: "port",
+    key: "port",
+  },
+  {
+    title: "工号",
+    dataIndex: "jobNum",
+    key: "jobNum",
+  },
   {
     title: "姓名",
     dataIndex: "name",
     key: "name",
   },
   {
-    title: "年龄",
-    dataIndex: "age",
-    key: "age",
+    title: "职务",
+    dataIndex: "job",
+    key: "job",
+  },
+  {
+    title: "生成密码",
+    dataIndex: "password",
+    key: "password",
+  },
+  {
+    title: "手机号",
+    dataIndex: "tel",
+    key: "tel",
   },
   {
     title: "操作",
     key: "action",
   },
 ]
+const formRef = ref()
+const isEdit = ref<string>("新增")
+const open = ref<boolean>(false)
+const formState = ref<FormState>({
+  name: "",
+  jobNum: "",
+  job: "",
+  tel: "",
+  port: "",
+})
+const showModal = () => {
+  formState.value = { name: "", jobNum: "", job: "", tel: "", port: "" }
+  isEdit.value = "新增"
+  open.value = true
+}
+const handleOk = async () => {
+  formRef.value
+    .validateFields()
+    .then(async (res: FormState) => {
+      console.log(res, "ff")
+      open.value = false
+      if (isEdit.value == "新增") {
+        addStaff()
+        getStaffList()
+      } else {
+        let result = await request.post("/api/staff/staffEdit", formState.value)
+        console.log(result)
+        getStaffList()
+      }
+    })
+    .catch((err: any) => {
+      console.log(err)
+    })
+}
 const data = ref([])
 const searchVal = ref<string>("")
-
 const onSearch = async (searchValue: string) => {
   console.log("or use this.value", searchVal.value)
-  let result = await request.get("/api/user/search", {
+  let result = await request.get("/api/staff/staffSearch", {
     params: {
       searchVal: searchValue,
     },
@@ -47,26 +129,34 @@ const onSearch = async (searchValue: string) => {
   console.log(result)
   data.value = result.data.data
 }
-const getUserList = async () => {
-  let result = await request.get("/api/staff/list")
-  console.log(result)
+const getStaffList = async () => {
+  let result = await request.get("/api/staff/staffList")
   data.value = result.data.data
 }
-const delUser = async (uid: number) => {
-  let result = await request.get("/api/user/delete", {
+const delStaff = async (id: number) => {
+  let result = await request.get("/api/staff/staffDel", {
     params: {
-      id: uid,
+      id,
     },
   })
   console.log(result)
-  getUserList()
+  getStaffList()
+}
+const editStaff = async (value: FormState) => {
+  isEdit.value = "编辑"
+  open.value = true
+  formState.value = value
+}
+const addStaff = async () => {
+  let result = await request.post("/api/staff/staffAdd", formState.value)
+  console.log(result)
 }
 const reset = () => {
   searchVal.value = ""
-  getUserList()
+  getStaffList()
 }
 onMounted(() => {
-  getUserList()
+  getStaffList()
 })
 </script>
 <style lang="less" scoped>
